7b5d429fab5677989553c721ac194c28fb57d26c
Joffrey Demetz <joffrey.demetz@gmail.com>
2025-08-26 15:40:19 +0200

    manage build less variables

--- config/jpack.js
+++ config/jpack.js
@@ -2,7 +2,24 @@ import fs from 'fs';
 import path from 'path';
 import { LogMe, jPackConfig } from 'jizy-packer';
 
+function generateLessVariablesFromConfig(variablesPath) {
+    LogMe.log('Build lib/less/_variables.less');
+    const desktopBreakpoint = jPackConfig.get('desktopBreakpoint') ?? '768px';
+    let content = `@desktop-breakpoint: ${desktopBreakpoint};` + "\n";
+    content += `@mobile-breakpoint: @desktop-breakpoint - 1px;`;
+    fs.writeFileSync(variablesPath, content, { encoding: 'utf8' });
+}
+
+function deleteLessVariablesFile(variablesPath) {
+    if (fs.existsSync(variablesPath)) {
+        LogMe.log('Delete lib/less/_variables.less');
+        fs.unlinkSync(variablesPath);
+    }
+}
+
 const jPackData = function () {
+    const lessBuildVariablesPath = path.join(jPackConfig.get('basePath'), 'lib', 'less', '_variables.less');
+
     jPackConfig.sets({
         name: 'BrowserCompat',
         alias: 'jizy-browser',
@@ -12,18 +29,16 @@ const jPackData = function () {
     jPackConfig.set('onCheckConfig', () => { });
 
     jPackConfig.set('onGenerateBuildJs', (code) => {
-        LogMe.log('Generate config.less');
-        LogMe.log('  path: ' + path.join(jPackConfig.get('targetPath'), 'config.less'));
-        const desktopBreakpoint = jPackConfig.get('desktopBreakpoint') ?? '768px';
-        let lessContent = `@desktop-breakpoint: ${desktopBreakpoint};` + "\n";
-        lessContent += `@mobile-breakpoint: @desktop-breakpoint - 1px;`;
-        fs.writeFileSync(path.join(jPackConfig.get('targetPath'), 'config.less'), lessContent);
+        generateLessVariablesFromConfig(lessBuildVariablesPath);
+
         return code;
     });
 
     jPackConfig.set('onGenerateWrappedJs', (wrapped) => wrapped);
 
-    jPackConfig.set('onPacked', () => { });
+    jPackConfig.set('onPacked', () => {
+        deleteLessVariablesFile(lessBuildVariablesPath);
+    });
 };
 
 export default jPackData;
